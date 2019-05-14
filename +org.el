39d00852e6ae73125bb816d678ced8a4bbf4eb7b
Jiayuan <forrestchang7@gmail.com>
2019-05-14 15:20:33 +0800

    feat(org): update org mode configs

--- +org.el
+++ +org.el
@@ -1,16 +1,9 @@
 ;;; ~/.doom.d/_org.el -*- lexical-binding: t; -*-
 
-(setq org-agenda-files '("~/Dropbox/personal-site/workspace/org/"))
 
-(setq org-agenda-span 'day)
-
-(setq org-log-into-drawer t)
-
-;; Org refile
-(setq org-refile-use-outline-path 'file)
-(setq org-outline-path-complete-in-steps nil)
-(setq org-refile-targets
-      '((org-agenda-files . (:maxlevel . 4))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; UI Settings
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Org headlines style
 (after! org
@@ -20,3 +13,41 @@
                   org-level-6 org-level-7
                   org-level-8))
     (set-face-attribute face nil :weight 'normal)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Org Agenda
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq org-agenda-files '("~/Dropbox/personal-site/workspace/org/"))
+
+(setq org-agenda-span 'day)
+
+(setq org-log-into-drawer t)
+
+(setq org-agenda-archives-mode t)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Org Refile
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq org-refile-use-outline-path 'file)
+
+(setq org-outline-path-complete-in-steps nil)
+
+(setq org-refile-targets
+      '((org-agenda-files . (:maxlevel . 4))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Org Capture
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq org-capture-templates
+      '(
+        ("j" "Journal" entry (file+datetree "~/Dropbox/personal-site/workspace/org/journal.org")
+         "* %?\nCaptured on %U\n%i\n%a")
+        ("t" "Todo" entry (file+headline "~/Dropbox/personal-site/workspace/org/inbox.org" "Todos")
+         "* TODO %?\n:PROPERTIES:\n:CREATED: %U\n:END:")
+        ))
