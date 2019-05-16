d1221beaaab4141395979170b2269db6905c43d5
Jiayuan <forrestchang7@gmail.com>
2019-05-16 20:10:53 +0800

    feat: add python configs
    - use lsp mode
    - use mypy and pylint to check errors

--- +python.el
+++ +python.el
@@ -1,6 +1,13 @@
 ;;; ~/.doom.d/+python.el -*- lexical-binding: t; -*-
 
-(setq python-shell-interpreter "python3"
-      flycheck-python-pycompile-executable "python3")
+(setq python-shell-interpreter "~/venv/python3.7/bin/python"
+      flycheck-python-pycompile-executable "~/venv/python3.7/bin/python"
+      flycheck-python-pylint-executable "~/venv/python3.7/bin/pylint"
+      flycheck-python-mypy-executable "~/venv/python3.7/bin/mypy")
 
 (setq anaconda-mode-localhost-address "localhost")
+
+;; pylint check
+(add-hook 'python-mode-hook
+          (lambda ()
+            (flycheck-add-next-checker 'python-pylint 'python-mypy)))
