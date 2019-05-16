48a4d66028ec6a12976889304201d1b1346c0c6c
Jiayuan <forrestchang7@gmail.com>
2019-05-16 20:12:07 +0800

    feat: add web mode support

--- init.el
+++ init.el
@@ -134,7 +134,7 @@
        (sh +fish)        ; she sells (ba|z|fi)sh shells on the C xor
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
-       ;;web               ; the tubes
+       web               ; the tubes
        ;;vala              ; GObjective-C
 
        ;; Applications are complex and opinionated modules that transform Emacs
