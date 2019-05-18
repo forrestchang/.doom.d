4539644443cdd8dfbe81bb4d280afc8b848a711b
Jiayuan <forrestchang7@gmail.com>
2019-05-18 20:18:18 +0800

    feat: user Sarasa Mono SC as fixed width font

--- +editor.el
+++ +editor.el
@@ -1,7 +1,30 @@
 ;;; ~/.doom.d/+editor.el -*- lexical-binding: t; -*-
 
-(setq doom-font
-      (font-spec :family "Fira Code Retina" :size 17))
+;; (setq doom-font
+;;       (font-spec :family "Sarasa Mono SC" :size 17 :weight 'normal))
+
+;; https://blog.csdn.net/xh_acmagic/article/details/78939246
+(defun +my/better-font()
+  (interactive)
+  ;; english font
+  (if (display-graphic-p)
+      (progn
+        (set-face-attribute 'default nil :font (format   "%s:pixelsize=%d" "Sarasa Mono SC" 18)) ;; 11 13 17 19 23
+        ;; chinese font
+        (dolist (charset '(kana han symbol cjk-misc bopomofo))
+          (set-fontset-font (frame-parameter nil 'font)
+                            charset
+                            (font-spec :family "Sarasa Mono SC")))) ;; 14 16 20 22 28
+    ))
+
+(defun +my|init-font(frame)
+  (with-selected-frame frame
+    (if (display-graphic-p)
+        (+my/better-font))))
+
+(if (and (fboundp 'daemonp) (daemonp))
+    (add-hook 'after-make-frame-functions #'+my|init-font)
+  (+my/better-font))
 
 ;; Use avy jump for all windows
 (setq avy-all-windows t)
