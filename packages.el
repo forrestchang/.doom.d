7461f514e07b64acad322396f64a827504f2d1ae
Jiayuan <forrestchang7@gmail.com>
2019-05-23 13:58:13 +0800

    fix: remove pyim package in custom packages
    Using official chinese pacakge

--- packages.el
+++ packages.el
@@ -6,7 +6,6 @@
 ;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
 
-(package! pyim)
 (package! writeroom-mode)
 (package! org-pomodoro)
 (package! py-isort)
