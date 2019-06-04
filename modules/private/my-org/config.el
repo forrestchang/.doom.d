a43389aff34202f15b586518a1046811d8b5df50
Jiayuan <forrestchang7@gmail.com>
2019-06-04 21:27:05 +0800

    fix: change org gcal file name

--- modules/private/my-org/config.el
+++ modules/private/my-org/config.el
@@ -42,7 +42,7 @@
               org-gcal-file-alist '(("forrestchang7@gmail.com" . "~/Dropbox/org/calendars/gcal_default.org")
                                     ("addressbook#contacts@group.v.calendar.google.com" . "~/Dropbox/org/calendars/gcal_holidays.org")
                                     ("o2lcgjdtg3h441i6m1ddugiee4@group.calendar.google.com" . "~/Dropbox/org/calendars/gcal_hobby.org")
-                                    ("b7tj496ae8b76pbfn1utoen58o@group.calendar.google.com" . "~/Dropbox/org/calendars/gcal_learning.org")
+                                    ("b7tj496ae8b76pbfn1utoen58o@group.calendar.google.com" . "~/Dropbox/org/calendars/gcal_learn.org")
                                     ("8eahnd4d5p3avqfbnhn0cgcqq0@group.calendar.google.com" . "~/Dropbox/org/calendars/gcal_life.org")
                                     ("3j9ok2brthfbpdojuj4vtr0hq0@group.calendar.google.com" . "~/Dropbox/org/calendars/gcal_system.org")
                                     ("f39mckhr7gdtp2evivta86gvbk@group.calendar.google.com" . "~/Dropbox/org/calendars/gcal_work.org")
