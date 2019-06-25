746c19ba8f4036e262fc7b5533874cf04a02f311
Jiayuan <forrestchang7@gmail.com>
2019-06-25 14:11:03 +0800

    feat: use microsoft python language server

--- modules/private/my-python/config.el
+++ modules/private/my-python/config.el
@@ -1,7 +1,9 @@
 ;;; private/my-python/config.el -*- lexical-binding: t; -*-
 
 (def-package! lsp-python-ms
+  :demand
+  :hook (python-mode . lsp)
   :config
-  (setq lsp-python-ms-dir
-        (expand-file-name "~/Github/python-language-server/output/bin/Release/"))
+  (setq lsp-python-ms-executable
+        "~/.local/bin/Microsoft.Python.LanguageServer")
   )
