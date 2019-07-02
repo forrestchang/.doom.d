c6c64247ceb51720828a043b82c7807d6f07633d
Jiayuan <forrestchang7@gmail.com>
2019-07-02 17:39:38 +0800

    feat: update lsp-python-ms settings

--- modules/private/my-python/config.el
+++ modules/private/my-python/config.el
@@ -1,16 +1,11 @@
 ;;; private/my-python/config.el -*- lexical-binding: t; -*-
 
 (def-package! lsp-python-ms
-  :demand
-  :hook (python-mode . lsp)
+  :hook (python-mode . (lambda ()
+                         (require 'lsp-python-ms)
+                         (lsp-deferred)))
   :config
-  (setq lsp-python-ms-executable "~/.local/bin/Microsoft.Python.LanguageServer"
-        lsp-python-ms-extra-paths '("/Users/jiayuan/Developer/iqiyi/topic_admin/backend"
+  (setq lsp-python-ms-extra-paths '("/Users/jiayuan/Developer/iqiyi/topic_admin/backend"
                                     "/Users/jiayuan/Developer/iqiyi/topic_admin/backend/app/api"
-                                    "/Users/jiayuan/Developer/iqiyi/topic_admin/backend/app/job")
-        lsp-ui-doc-enable nil
-        lsp-ui-peek-enable nil
-        lsp-ui-sideline-enable nil
-        lsp-ui-imenu-enable nil
-        lsp-ui-flycheck-enable t)
+                                    "/Users/jiayuan/Developer/iqiyi/topic_admin/backend/app/job"))
   )
