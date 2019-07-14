31c9202913360991d82fcad841bdb3f18efd1c1f
Jiayuan <forrestchang7@gmail.com>
2019-07-14 22:21:41 +0800

    feat: add function to increment number

--- config.el
+++ config.el
@@ -164,3 +164,21 @@ unwanted space when exporting org-mode to hugo markdown."
 ;; company-lsp
 (after! company-lsp
   (setq company-lsp-cache-candidates 'auto))
+
+
+(defun my-increment-number-decimal (&optional arg)
+  "Increment the number forward from point by 'arg'."
+  (interactive "p*")
+  (save-excursion
+    (save-match-data
+      (let (inc-by field-width answer)
+        (setq inc-by (if arg arg 1))
+        (skip-chars-backward "0123456789")
+        (when (re-search-forward "[0-9]+" nil t)
+          (setq field-width (- (match-end 0) (match-beginning 0)))
+          (setq answer (+ (string-to-number (match-string 0) 10) inc-by))
+          (when (< answer 0)
+            (setq answer (+ (expt 10 field-width) answer)))
+          (replace-match (format (concat "%0" (int-to-string field-width) "d")
+                                 answer)))))))
+(global-set-key (kbd "C-c +") 'my-increment-number-decimal)
