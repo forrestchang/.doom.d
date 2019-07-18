25fabf2e2b46755f8b15759ec8b1a0b074303719
Jiayuan <forrestchang7@gmail.com>
2019-07-18 09:46:01 +0800

    feat: enable company-box-doc

--- config.el
+++ config.el
@@ -41,7 +41,7 @@
 ;; Company
 (after! company
   (setq company-idle-delay 0
-        company-box-doc-enable nil
+        company-box-doc-enable t
         company-box-enable-icon nil))
 
 ;; Swiper search
