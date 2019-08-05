d2321bac356bffd4339fdbda016c26582712c09c
Jiayuan <forrestchang7@gmail.com>
2019-08-06 01:19:34 +0800

    feat: copy org item link in org-agenda

--- +org.el
+++ +org.el
@@ -130,6 +130,27 @@
 (setq org-id-link-to-org-use-id 'create-if-interactive-and-no-custom-id
       org-clone-delete-id t)
 
+;; Copy org item link in org agenda mode
+;; Source: https://koenig-haunstetten.de/2018/02/17/improving-my-orgmode-workflow/
+(defun my/copy-idlink-to-clipboard()
+  "Copy an ID link with the headline to killring, if no ID is there then create
+a new unique ID. This function works only in org-mode or org-agenda buffers.
+
+The purpose of this function is to easily construct id:-links to org-mode items.
+If its assigned to a key it saves you marking the text and copying to the
+killring."
+  (interactive)
+  (when (eq major-mode 'org-agenda-mode) ;switch to orgmode
+    (org-agenda-show)
+    (org-agenda-goto))
+  (when (eq major-mode 'org-mode)       ; do this only in org-mode buffers
+    (setq mytmphead (nth 4 (org-heading-components)))
+    (setq mytmpid (funcall 'org-id-get-create))
+    (setq mytmplink (format "[[id:%s][%s]]" mytmpid mytmphead))
+    (kill-new mytmplink)
+    (message "Copied %s to killring (clipboard)" mytmplink)
+    ))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Org Agenda
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
