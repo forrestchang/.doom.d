f5076924df206624bf15ba3656330f3f0764036a
Jiayuan <forrestchang7@gmail.com>
2019-09-24 14:43:12 +0800

    fix: disable company box doc

--- config.el
+++ config.el
@@ -35,7 +35,7 @@
 ;; Company
 (after! company
   (setq company-idle-delay 0
-        company-box-doc-enable t
+        company-box-doc-enable nil
         company-box-enable-icon nil))
 
 ;; Swiper search
